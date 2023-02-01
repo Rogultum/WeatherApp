bd338732ddf19056be2ffdd9b444a6420e2c463f
Remzi Ogul TUM <amadeuwoles@gmail.com>
2023-02-01 18:31:01 +0300

    add line chart with mock data and refactor styles

--- app/screens/WeatherScreen.tsx
+++ app/screens/WeatherScreen.tsx
@@ -1,58 +1,138 @@
-import React from "react"
-// import { observer } from "mobx-react-lite"
-import { ViewStyle, View, TextStyle, ImageStyle } from "react-native"
-// import { StackScreenProps } from "@react-navigation/stack"
-// import { AppStackScreenProps } from "../navigators"
-import { AutoImage, Icon, Screen, Text } from "../components"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../models"
+import { Skia, SkPath, Canvas, Line, vec, Path } from "@shopify/react-native-skia"
+import React, { useState } from "react"
 
-// STOP! READ ME FIRST!
-// To fix the TS error below, you'll need to add the following things in your navigation config:
-// - Add `<%= props.pascalCaseName %>: undefined` to AppStackParamList
-// - Import your screen, and add it to the stack:
-//     `<Stack.Screen name="<%= props.pascalCaseName %>" component={<%= props.pascalCaseName%>Screen} />`
-// Hint: Look for the 🔥!
+import { scaleLinear, scaleTime, line, curveBasis } from "d3"
+
+import { ViewStyle, View, TextStyle, ImageStyle, Image } from "react-native"
+
+import { AutoImage, Icon, Screen, Text } from "../components"
+import { DataPoint, originalData } from "../data"
+
+// to get current weather for London: JSON: http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=London
+// XML: http://api.weatherapi.com/v1/current.xml?key=<YOUR_API_KEY>&q=London
+
+// To get 7 day weather for US Zipcode 07112: JSON: http://api.weatherapi.com/v1/forecast.json?key=<YOUR_API_KEY>&q=07112&days=7
+// XML: http://api.weatherapi.com/v1/forecast.xml?key=<YOUR_API_KEY>&q=07112&days=7
+
+// Search for cities starting with Lond: JSON: http://api.weatherapi.com/v1/search.json?key=<YOUR_API_KEY>&q=lond
+// XML: http://api.weatherapi.com/v1/search.xml?key=<YOUR_API_KEY>&q=lond
+
+import { BASE_URL, API_KEY } from "../.env/WeatherApi"
+import axios from "axios"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+interface GraphData {
+  max: number
+  min: number
+  curve: SkPath
+}
 
 export const WeatherScreen = () => {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const GRAPH_HEIGHT = 200
+  const GRAPH_WIDTH = 370
 
-  // Pull in navigation via hook
-  //   const navigation = useNavigation()
+  const [weather, setWeather] = useState([])
 
-  // source={require("../../assets/images/sun.png")}
+  const makeGraph = (data: DataPoint[]): GraphData => {
+    const min = Math.min(...data.map((val) => val.value))
+    const max = Math.max(...data.map((val) => val.value))
+
+    const getYAxis = scaleLinear().domain([0, max]).range([GRAPH_HEIGHT, 35])
+
+    const getXAxis = scaleTime()
+      .domain([new Date(2000, 1, 1), new Date(2000, 1, 15)])
+      .range([10, GRAPH_WIDTH - 10])
+
+    const curvedLine = line<DataPoint>()
+      .x((d) => getXAxis(new Date(d.date)))
+      .y((d) => getYAxis(d.value))
+      .curve(curveBasis)(data)
+
+    const skPath = Skia.Path.MakeFromSVGString(curvedLine)
+
+    return {
+      min,
+      max,
+      curve: skPath,
+    }
+  }
+
+  const graphData = makeGraph(originalData)
+
+  const getCurrentWeather = async () => {
+    await axios
+      .get(`${BASE_URL}/forecast.json?key=${API_KEY}&q=LONDON&aqi=yes`)
+      .then((response) => {
+        setWeather(response.data.forecast.forecastday.hour)
+      })
+    console.log(weather)
+
+    // console.log(
+    //   weather.map((h) => {
+    //     console.log(h)
+    //   }),
+    // )
+  }
+
   return (
     <Screen contentContainerStyle={$container} safeAreaEdges={["top"]} preset="scroll">
-      <View style={$container}>
-        <Text style={$degree} text="35°" size="xxl" />
-        <AutoImage style={$image} maxWidth={300} source={require("../../assets/images/sun.png")} />
-        <Text text="ANK" size="xl" />
-        <View style={$iconContainer}>
-          <Icon containerStyle={$icon} size={32} icon="humidity" />
-          <Icon containerStyle={$icon} size={32} icon="wind" />
-          <Icon containerStyle={$icon} size={32} icon="air" />
+      <SafeAreaView style={$container}>
+        <View style={$degreeContainer}>
+          <Text onPress={getCurrentWeather} style={$degreeNum} text="35" size="xxl" />
+          <Text text="°" size="xl" style={$degreeSym} />
         </View>
-      </View>
+        <AutoImage style={$image} maxWidth={250} source={require("../../assets/images/sun.png")} />
+        <Image style={$image} source={require("../../assets/images/sun.png")} />
+        <Text text="ANK" size="xl" style={$city} />
+        <View style={$iconContainer}>
+          <Icon containerStyle={$icon} size={26} icon="humidity" />
+          <Icon containerStyle={$icon} size={26} icon="wind" />
+          <Icon containerStyle={$icon} size={26} icon="air" />
+        </View>
+        <Canvas
+          style={[
+            {
+              height: GRAPH_HEIGHT,
+              width: GRAPH_WIDTH,
+            },
+            $canvas,
+          ]}
+        >
+          <Line strokeWidth={0.5} color="lightGrey" p1={vec(10, 130)} p2={vec(400, 130)} />
+          <Path strokeWidth={2.5} color="black" path={graphData.curve} style="stroke" />
+        </Canvas>
+      </SafeAreaView>
     </Screen>
   )
 }
 
 const $container: ViewStyle = {
   flex: 1,
-
   justifyContent: "center",
   alignItems: "center",
   paddingTop: 5,
   paddingHorizontal: 5,
 }
-const $degree: TextStyle = {
-  zIndex: 2,
+const $degreeContainer: ViewStyle = {
+  flexDirection: "row",
 }
+const $degreeNum: TextStyle = {
+  zIndex: 2,
+  top: -70,
+}
+const $degreeSym: TextStyle = {
+  zIndex: 2,
+  top: -70,
+  position: "absolute",
+  right: -55,
+}
+const $city: TextStyle = {}
 const $iconContainer: ViewStyle = {
   width: "100%",
   flexDirection: "row",
   justifyContent: "space-evenly",
+  zIndex: 2,
+  bottom: -80,
 }
 const $icon: ViewStyle = {
   borderWidth: 0.7,
@@ -62,4 +142,11 @@ const $icon: ViewStyle = {
 const $image: ImageStyle = {
   zIndex: 1,
   position: "absolute",
+  opacity: 0.7,
+  maxWidth: 400,
+  resizeMode: "contain",
+  top: -100,
+}
+const $canvas = {
+  bottom: -35,
 }
